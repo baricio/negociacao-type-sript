069a7fefef9b1e8d35204db64bc4957759bb6bca
Fabricio Cunha <fabricio.cunha@pdmfc.com>
2020-04-10 17:46:47 +0100

    created a type script definition to jquery

--- app/js/controllers/NegociacaoController.js
+++ app/js/controllers/NegociacaoController.js
@@ -2,15 +2,15 @@ class NegociacaoController {
     constructor() {
         this._negociacoes = new Negociacoes();
         this._negociacoesView = new NegociacoesView('#table');
-        this._menssagemView = new MensagemView('#mensagemView');
-        this._inputData = document.querySelector('#data');
-        this._inputQuantidade = document.querySelector('#quantidade');
-        this._inputValor = document.querySelector('#valor');
+        this._menssagemView = new MenssagemView('#mensagemView');
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
         this._negociacoesView.update(this._negociacoes);
     }
     adiciona(event) {
         event.preventDefault();
-        const negociacao = new Negociacao(new Date(this._inputData.value.replace(/-/g, ',')), parseInt(this._inputQuantidade.value), parseFloat(this._inputValor.value));
+        const negociacao = new Negociacao(new Date(this._inputData.val().replace(/-/g, ',')), parseInt(this._inputQuantidade.val()), parseFloat(this._inputValor.val()));
         this._negociacoes.adiciona(negociacao);
         this._negociacoesView.update(this._negociacoes);
         this._menssagemView.update('Negociação adicionada com sucesso!');
